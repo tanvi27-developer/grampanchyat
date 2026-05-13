6b954e34e2d36069c7489b124a236bdd898682c7
Tanvi Jadhav <tanvi272329@gmail.com>
2026-05-13 11:49:59 +0530

    fixed static assets issue

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+   app.UseStaticFiles();
 
 app.MapRazorPages();
 
